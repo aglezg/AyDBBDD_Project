974d5338d9507b7360e595718a4aceb8bc7bec28
Adrián González Galván <glezgalvan.adrian@gmail.com>
2023-12-14 16:38:54 +0000

    Adding initialize script

---
+++ scripts/initialize.sql
@@ -0,0 +1,8 @@
+-- Dropping existing database
+DROP DATABASE IF EXISTS mylibrary;
+
+-- Creating database 'myLibrary'
+CREATE DATABASE mylibrary;
+
+-- Connecting to created database
+\connect mylibrary
